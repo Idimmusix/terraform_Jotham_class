e020de4aad131ebe485c0369080725688c7a8bf8
Deji Adegbite <d2alphame@yahoo.com>
2024-09-07 18:44:33 -0400

    created an IAC to provision google_cloud_insance e2-micro

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "google" {
+    credentials = file(var.credentials)
+    project     = "jotham-class-7"
+    region      = "us-central1"
+}
+
+resource "google_compute_instance" "be_instance" {
+
+  name         = "terraform-vm"
+
+  machine_type = "e2-micro"
+
+  zone         = "us-central1-a"
+
+
+  boot_disk {
+
+    initialize_params {
+
+      image = "debian-cloud/debian-11"
+
+    }
+
+  }
+
+
+  network_interface {
+
+    network = "default"
+
+    access_config {}
+
+  }
+
+}
+
+output "instance_ip" {
+    value = google_compute_instance.be_instance.network_interface[0].access_config[0].nat_ip
+}
+
+variable "credentials" {
+    type = string
+    description = "This crednetials is the best thing after sliced bread"
+    default = "C:\\Users\\MR DEJI\\idimma\\jotham\\terraform\\jotham-class-7-8fa445ebbd11.json"
+}
